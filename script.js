087c0aa9fe51092fb8e95f40f0412b9f772fda20
sudhay23 <sudhay2001@gmail.com>
2021-12-17 17:15:08 +0530

    App done. Ready for exploits :)

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-import { initialiseApp } from "firebase/app";
+const firebaseConfig = {
+    apiKey: "AIzaSyA4SQqCf3U2vvTr_Bkbfl3bHZnGNxr1SMI",
+    authDomain: "inductionio-event.firebaseapp.com",
+    projectId: "inductionio-event",
+    storageBucket: "inductionio-event.appspot.com",
+    messagingSenderId: "101950700286",
+    appId: "1:101950700286:web:96dd44a34780fe43c143af",
+    measurementId: "G-4DYMK46ZVJ",
+};
+
+// Init app
+firebase.initializeApp(firebaseConfig);
+const firestore = firebase.firestore();
+
+const reviewForm = document.querySelector("form");
+const successMessage = document.querySelector(".success-message");
+
+// Submitting the form
+reviewForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    successMessage.classList.remove("show");
+
+    let name = document.getElementById("nameInput").value;
+    let productName = document.getElementById("productNameInput").value;
+    let feedback = document.getElementById("feedbackInput").value;
+
+    firestore
+        .collection("Reviews")
+        .doc()
+        .set({
+            name,
+            productName,
+            feedback,
+        })
+        .then(() => {
+            successMessage.classList.add("show");
+            reviewForm.reset();
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+});
